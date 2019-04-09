6ed009e3f259f9adfeff39f4971e65df5aa85671
heiyedeshengyin <270193571@qq.com>
2019-04-09 11:18:27 +0800

    重载所有比较运算符

--- BigNum/BigNum.h
+++ BigNum/BigNum.h
@@ -15,8 +15,22 @@ private:
 public:
 	bignum();
 	bignum(const char* str);
+	bignum(int _num);
+	bignum(unsigned long _num);
+	bignum(long long _num);
 	bignum(const bignum& ano);
 	~bignum();
+	bool isPositive();
+	bool isZero();
+	int getLength();
+	void show();
+	bignum& operator=(const bignum& copy_num);
+	bool operator==(const bignum& compare_num);
+	bool operator!=(const bignum& compare_num);
+	bool operator>(const bignum& compare_num);
+	bool operator<(const bignum& compare_num);
+	bool operator>=(const bignum& compare_num);
+	bool operator<=(const bignum& compare_num);
 };
 
 bignum::bignum()
@@ -70,8 +84,10 @@ bignum::bignum(const char* str)
 			length = str_length - zero_count - 1;
 			if (length == 0)
 			{
+				cout << "构造函数错误,字符串不能都是0" << endl;
 				num = new int[1];
 				num[0] = 0;
+				length = 1;
 				is_positive = true;
 			}
 			else
@@ -108,8 +124,10 @@ bignum::bignum(const char* str)
 			length = str_length - zero_count;
 			if (length == 0)
 			{
+				cout << "构造函数错误,字符串不能都是0" << endl;
 				num = new int[1];
 				num[0] = 0;
+				length = 1;
 				is_positive = true;
 			}
 			else
@@ -134,6 +152,141 @@ bignum::bignum(const char* str)
 		}
 }
 
+bignum::bignum(int _num)
+{
+	if (_num < -2147483647 || _num > 2147483647)
+	{
+		cout << "构造函数异常,int类型参数超出范围" << endl;
+		num = new int[1];
+		num[0] = 0;
+		length = 1;
+		is_positive = true;
+
+	}
+	else
+	{
+		int m_num = _num;
+		int n_num = _num;
+		int m_length = 0;
+
+		if (m_num < 0)
+		{
+			m_num = 0 - m_num;
+			n_num = 0 - n_num;
+			is_positive = false;
+		}
+		else
+		{
+			is_positive = true;
+		}
+
+		while (m_num > 0)
+		{
+			m_length++;
+			m_num /= 10;
+		}
+
+		if (m_length == 0)
+		{
+			num = new int[1];
+			num[0] = 0;
+			length = 1;
+		}
+		else
+		{
+			num = new int[m_length];
+			length = m_length;
+			for (int i = 0; i < length; i++)
+			{
+				num[i] = n_num % 10;
+				n_num /= 10;
+			}
+		}
+	}
+}
+
+bignum::bignum(unsigned long _num)
+{
+	unsigned long m_num = _num;
+	unsigned long n_num = _num;
+	int m_length = 0;
+
+	if (m_num < 0)
+	{
+		m_num = 0 - m_num;
+		n_num = 0 - n_num;
+		is_positive = false;
+	}
+	else
+	{
+		is_positive = true;
+	}
+
+	while (m_num > 0)
+	{
+		m_length++;
+		m_num /= 10;
+	}
+
+	if (m_length == 0)
+	{
+		num = new int[1];
+		num[0] = 0;
+		length = 1;
+	}
+	else
+	{
+		num = new int[m_length];
+		length = m_length;
+		for (int i = 0; i < length; i++)
+		{
+			num[i] = n_num % 10;
+			n_num /= 10;
+		}
+	}
+}
+
+bignum::bignum(long long _num)
+{
+	long long m_num = _num;
+	long long n_num = _num;
+	int m_length = 0;
+
+	if (m_num < 0)
+	{
+		m_num = 0 - m_num;
+		n_num = 0 - n_num;
+		is_positive = false;
+	}
+	else
+	{
+		is_positive = true;
+	}
+
+	while (m_num > 0)
+	{
+		m_length++;
+		m_num /= 10;
+	}
+
+	if (m_length == 0)
+	{
+		num = new int[1];
+		num[0] = 0;
+		length = 1;
+	}
+	else
+	{
+		num = new int[m_length];
+		length = m_length;
+		for (int i = 0; i < length; i++)
+		{
+			num[i] = n_num % 10;
+			n_num /= 10;
+		}
+	}
+}
+
 bignum::bignum(const bignum& ano)
 {
 	length = ano.length;
@@ -146,7 +299,264 @@ bignum::bignum(const bignum& ano)
 
 bignum::~bignum()
 {
+	delete[] num;
+}
+
+bool bignum::isPositive()
+{
+	return is_positive;
+}
+
+bool bignum::isZero()
+{
+	return length == 1 && num[0] == 0;
+}
+
+int bignum::getLength()
+{
+	return length;
+}
+
+void bignum::show()
+{
+	if (!is_positive)
+		cout << "-";
+
+	for (int i = length - 1; i >= 0; i--)
+		cout << num[i];
+
+	cout << endl;
+}
+
+bignum& bignum::operator=(const bignum& copy_num)
+{
+	if (this == &copy_num)
+		return *this;
+
+	length = copy_num.length;
+	is_positive = copy_num.is_positive;
 	delete num;
+	num = new int[length];
+	for (int i = 0; i < length; i++)
+		num[i] = copy_num.num[i];
+
+	return *this;
+}
+
+bool bignum::operator==(const bignum& compare_num)
+{
+	if (length != compare_num.length || is_positive != compare_num.is_positive)
+		return false;
+	else
+	{
+		for (int i = 0; i < length; i++)
+			if (num[i] != compare_num.num[i])
+				return false;
+
+		return true;
+	}
+}
+
+bool bignum::operator!=(const bignum& compare_num)
+{
+	if (length != compare_num.length || is_positive != compare_num.is_positive)
+		return true;
+	else
+	{
+		for (int i = 0; i < length; i++)
+			if (num[i] != compare_num.num[i])
+				return true;
+
+		return false;
+	}
+}
+
+bool bignum::operator>(const bignum& compare_num)
+{
+	if (is_positive && !compare_num.is_positive)
+		return true;
+
+	if (!is_positive && compare_num.is_positive)
+		return false;
+
+	if (is_positive && compare_num.is_positive)
+	{
+		if (length > compare_num.length)
+			return true;
+		if (length < compare_num.length)
+			return false;
+		if (length == compare_num.length)
+		{
+			for (int i = length - 1; i >= 0; i--)
+			{
+				if (num[i] > compare_num.num[i])
+					return true;
+				if (num[i] < compare_num.num[i])
+					return false;
+			}
+			return false;
+		}
+	}
+
+	if (!is_positive && !compare_num.is_positive)
+	{
+		if (length > compare_num.length)
+			return false;
+		if (length < compare_num.length)
+			return true;
+		if (length == compare_num.length)
+		{
+			for (int i = length - 1; i >= 0; i--)
+			{
+				if (num[i] > compare_num.num[i])
+					return false;
+				if (num[i] < compare_num.num[i])
+					return true;
+			}
+			return false;
+		}
+	}
+}
+
+bool bignum::operator<(const bignum& compare_num)
+{
+	if (is_positive && !compare_num.is_positive)
+		return false;
+
+	if (!is_positive && compare_num.is_positive)
+		return true;
+
+	if (is_positive && compare_num.is_positive)
+	{
+		if (length > compare_num.length)
+			return false;
+		if (length < compare_num.length)
+			return true;
+		if (length == compare_num.length)
+		{
+			for (int i = length - 1; i >= 0; i--)
+			{
+				if (num[i] > compare_num.num[i])
+					return false;
+				if (num[i] < compare_num.num[i])
+					return true;
+			}
+			return false;
+		}
+	}
+
+	if (!is_positive && !compare_num.is_positive)
+	{
+		if (length > compare_num.length)
+			return true;
+		if (length < compare_num.length)
+			return false;
+		if (length == compare_num.length)
+		{
+			for (int i = length - 1; i >= 0; i--)
+			{
+				if (num[i] > compare_num.num[i])
+					return true;
+				if (num[i] < compare_num.num[i])
+					return false;
+			}
+			return false;
+		}
+	}
+}
+
+bool bignum::operator>=(const bignum& compare_num)
+{
+	if (is_positive && !compare_num.is_positive)
+		return true;
+
+	if (!is_positive && compare_num.is_positive)
+		return false;
+
+	if (is_positive && compare_num.is_positive)
+	{
+		if (length > compare_num.length)
+			return true;
+		if (length < compare_num.length)
+			return false;
+		if (length == compare_num.length)
+		{
+			for (int i = length - 1; i >= 0; i--)
+			{
+				if (num[i] > compare_num.num[i])
+					return true;
+				if (num[i] < compare_num.num[i])
+					return false;
+			}
+			return true;
+		}
+	}
+
+	if (!is_positive && !compare_num.is_positive)
+	{
+		if (length > compare_num.length)
+			return false;
+		if (length < compare_num.length)
+			return true;
+		if (length == compare_num.length)
+		{
+			for (int i = length - 1; i >= 0; i--)
+			{
+				if (num[i] > compare_num.num[i])
+					return false;
+				if (num[i] < compare_num.num[i])
+					return true;
+			}
+			return true;
+		}
+	}
+}
+
+bool bignum::operator<=(const bignum& compare_num)
+{
+	if (is_positive && !compare_num.is_positive)
+		return false;
+
+	if (!is_positive && compare_num.is_positive)
+		return true;
+
+	if (is_positive && compare_num.is_positive)
+	{
+		if (length > compare_num.length)
+			return false;
+		if (length < compare_num.length)
+			return true;
+		if (length == compare_num.length)
+		{
+			for (int i = length - 1; i >= 0; i--)
+			{
+				if (num[i] > compare_num.num[i])
+					return false;
+				if (num[i] < compare_num.num[i])
+					return true;
+			}
+			return true;
+		}
+	}
+
+	if (!is_positive && !compare_num.is_positive)
+	{
+		if (length > compare_num.length)
+			return true;
+		if (length < compare_num.length)
+			return false;
+		if (length == compare_num.length)
+		{
+			for (int i = length - 1; i >= 0; i--)
+			{
+				if (num[i] > compare_num.num[i])
+					return true;
+				if (num[i] < compare_num.num[i])
+					return false;
+			}
+			return true;
+		}
+	}
 }
 
 #endif // !BIGNUM_H
--- BigNum/Test.cpp
+++ BigNum/Test.cpp
@@ -1,11 +1,19 @@
 ﻿// BigNum.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
-#include <iostream>
+#include "BigNum.h"
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	bignum n1("4363460");
+	bignum n2("4363461");
+
+	if (n1 <= n2)
+		cout << "n1 <= n2" << endl;
+	else
+		cout << "n1 > n2" << endl;
+
+	return 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
